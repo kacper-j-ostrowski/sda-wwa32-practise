82f54d5f08cfde7465cf33b2d173322d86c615cb
lukaszkiraga <lukaszkiraga@gmail.com>
2021-10-17 09:55:27 +0200

    First commit lukasz_kiraga

--- src/main/java/pl/sda/practisegit/Main.java
+++ src/main/java/pl/sda/practisegit/Main.java
@@ -4,5 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+        int a =6;
+        String b = "Ibis";
+        int c = 8;
+
+        System.out.println(b+ " " + a+c);
     }
 }
